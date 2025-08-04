04583aadcdd471d4eeb68e376d9cda8ac7bf592c
ne_srv <ne_srv@mail.ru>
2025-08-04 14:21:39 +0300

    p1++

--- PRACTICE/lesson-7/p1.py
+++ PRACTICE/lesson-7/p1.py
@@ -1,18 +1,21 @@
 # print("Hello", "World", sep=' !!! ', file=open('test.txt', 'w'))
 
+import time
+
+
 def sum2(a, b):
     return a + b
 
 
-c = sum2(1, 2)
-print(c)
-result = sum2(sum2(4, -2), 7) + 12  # ?
-print(result)
+# c = sum2(1, 2)
+# print(c)
+# result = sum2(sum2(4, -2), 7) + 12  # ?
+# print(result)
 
 
 def my_abs_Galina(x):
     if x < 0:
-        x*(-1)
+        x = x*(-1)
     return x
 
 def my_abs_Dmitry(x):
@@ -22,6 +25,23 @@ def my_abs_Dmitry(x):
 def my_abs_Alex(x):
     return int(str(x).lstrip("-"))
 
+def my_abs(x):
+    return abs(x)
 
-x = my_abs_Galina(-10)
+
+t0 = time.time()
+for _ in range(10**7):
+    x =  my_abs(-10)
+
+t1= time.time()
+print(t1-t0) # 1.89 # 1.84  # 4.33 # 1.87
+
+
 print(x)
+
+# x =  my_abs_Dmitry(-10)
+# print(x)
+
+# x =  my_abs_Alex(-10)
+# print(x)
+
