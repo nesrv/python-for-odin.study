40de11c52c64ebf1d31a929d34763ef59565cdcf
ne_srv <ne_srv@mail.ru>
2025-08-08 14:25:30 +0300

    +

--- PRACTICE/lesson-9/p1.py
+++ PRACTICE/lesson-9/p1.py
@@ -1,12 +1,20 @@
 
 x = 0
+# x = 123,123
 
 print("Начало ...")
 
 try:
-    res = 10 / x
-    print(res)
-except ZeroDivisionError:
-    print ("Ошибка деления на ноль")
+    open("file.txt") 
+    res = 10 / x    
+except Exception as e:
+    print (e)
+    
+    
+# else:
+#     print("Все хорошо")
+# finally:
+#     print("Конец")
 
+print("Конец ...")
 
