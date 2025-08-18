2c9846a47e7782d2e898446f205994f90f8c1efd
nesrv <ne_srv@mail.ru>
2025-08-18 15:18:21 +0300

    +

--- PRACTICE/lesson-13/p2.py
+++ PRACTICE/lesson-13/p2.py
@@ -1,24 +1,9 @@
 from sys import *
 import subprocess
 
-print(version)
-print(platform)
 
-# print(argv[1], argv[2] + "!")
-
-print(executable)
-
-
-# subprocess.run("python --version")
-# code = subprocess.call(["python", "p1.py"])
-
-# print("p1.py вернул", code)
-
-path.append("C:/Users/user/Documents")
-# print (path)
-import test123
-print (test123.x)
-
-
-
-subprocess.call(["python", "calc"])
+if platform == "win32":
+    subprocess.call("mspaint")
+elif platform.startswith("linux"):
+    subprocess.call("gnome-calculator")
+    subprocess.Popen(["ls -l"])
---
+++ PRACTICE/lesson-13/p3.py
@@ -0,0 +1,7 @@
+import shutil
+# высокоуровневые операции для работы с файлами
+
+shutil.copy("txt.txt", "txt10.txt")
+
+shutil.copy2("txt.txt", "txt11.txt") # копирование с сохранением метаданных
+
