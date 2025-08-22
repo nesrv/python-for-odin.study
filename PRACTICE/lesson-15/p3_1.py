93e84257ec394683b6e09807479ca36065c763fc
nesrv <ne_srv@mail.ru>
2025-08-22 15:33:19 +0300

    +

--- PRACTICE/lesson-15/p3_1.py
+++ PRACTICE/lesson-15/p3_1.py
@@ -3,6 +3,11 @@ from sqlalchemy import *
 engine = create_engine('sqlite:///books.db')
 metadata = MetaData()
 
+authors = Table('authors', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(100), nullable=False),
+    Column('birth_year', Integer)
+)
 
 with engine.connect() as conn:
     # Добавляем авторов
@@ -15,3 +20,9 @@ with engine.connect() as conn:
         {'name': 'Иван Тургенев', 'birth_year': 1818},
         {'name': 'Николай Гоголь', 'birth_year': 1809}
     ]
+    
+    for author in authors_data:
+        conn.execute(insert(authors).values(**author))
+
+    conn.commit()
+    
